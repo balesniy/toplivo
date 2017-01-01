9b9966e6d21f8943f589d40d21bae3f860b8105a
balesniy <balesniy@yandex.ru>
2017-01-01 20:40:03 +0100

    initial

--- libs/passport/localStrategy.js
+++ libs/passport/localStrategy.js
@@ -4,7 +4,7 @@ module.exports = new Auth0Strategy({
   domain:       'balesniy.eu.auth0.com',
   clientID:     'DuFCvvnVdQU2dd2hJeA7JDEMykCT3tib',
   clientSecret: 'ebwCCzLPkqGvIvNXTo4wMxnJD-I7Ya3XP5RV0REBi0wRqyM2QsqGmb6yUM9VICDx',
-  callbackURL:  'http://localhost:3000/callback'
+  callbackURL:  'https://secret-tundra-34211.herokuapp.com/callback'
 }, function (accessToken, refreshToken, extraParams, profile, done) {
   // accessToken is the token to call Auth0 API (not needed in the most cases)
   // extraParams.id_token has the JSON Web Token
