decbb6be84db1bfdda2fa6205becb7ff6825cc88
balesniy <balesniy@yandex.ru>
2017-01-06 13:58:33 +0100

    payments collection

--- routes/admin.js
+++ routes/admin.js
@@ -3,14 +3,14 @@ exports.get = async function (ctx) {
   const users = await User.find({});
   ctx.body = ctx.render('admin', {
     users: users.map(user => user.getPublicFields())
-  })
+  });
 
-  if (ctx.isAuthenticated()) {
-
-  }
-  else {
-    ctx.body = ctx.render('login');
-  }
+  // if (ctx.isAuthenticated()) {
+  //
+  // }
+  // else {
+  //   ctx.body = ctx.render('login');
+  // }
 };
 
 exports.del = async function (ctx) {
