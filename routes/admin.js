e40c03e7dbdfa2626ab7d9c61adff8b0d090b59f
balesniy <balesniy@yandex.ru>
2017-01-06 13:51:21 +0100

    payments collection

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,12 @@
 const User = require('../models/user');
 exports.get = async function (ctx) {
+  const users = await User.find({});
+  ctx.body = ctx.render('admin', {
+    users: users.map(user => user.getPublicFields())
+  })
+
   if (ctx.isAuthenticated()) {
-    const users = await User.find({});
-    ctx.body = ctx.render('admin', {
-      users: users.map(user => user.getPublicFields())
-    })
+
   }
   else {
     ctx.body = ctx.render('login');
