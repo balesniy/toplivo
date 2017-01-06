0ba6387139bc78226e56dff42141dab1cffa1608
balesniy <balesniy@yandex.ru>
2017-01-06 14:14:04 +0100

    payments collection

--- routes/yandex.js
+++ routes/yandex.js
@@ -3,6 +3,6 @@ const hash = crypto.createHash('sha1');
 module.exports = async function (ctx) {
   const {sha1_hash, notification_type, operation_id, amount, currency, datetime, sender, codepro, label }=ctx.request.body
   const secret = '78Q2ko2OHNFqMrlSu7OI4Tct';
-  hash.update('some data to hash');
-  console.log(hash.digest('hex'), notification_type);
+  hash.update(`${notification_type}&${operation_id}&${amount}&${currency}&${datetime}&${sender}&${codepro}&${secret}&${label}`);
+  console.log(hash.digest('hex') == sha1_hash);
 };
